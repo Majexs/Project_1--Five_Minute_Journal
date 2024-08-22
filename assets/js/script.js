c8e2ff45b5737778d3fe01d7e76c79e67e1511be
Majexs <166327239+Majexs@users.noreply.github.com>
2024-08-21 21:38:13 -0400

    changed challenge to sleep

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@
 const dailyTracker = document.getElementById('daily_tracker');
 const arr = [
   ['Habits:', 'Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9', 'Day 10', 'Day 11', 'Day 12', 'Day 13', 'Day 14'], 
-  ['Challenged', '', '', '', '', '', '', '', '', '', '', '', '', '', ''], 
+  ['Slept', '', '', '', '', '', '', '', '', '', '', '', '', '', ''], 
   ['Hydrated', '', '', '', '', '', '', '', '', '', '', '', '', '', ''], 
   ['Learned', '', '', '', '', '', '', '', '', '', '', '', '', '', ''], 
   ['Walked', '', '', '', '', '', '', '', '', '', '', '', '', '', ''], 
